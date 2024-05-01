cce3e03b59c6689127f3177a4804a9475a454f38
Gireg <giregk@upsignon.eu>
2024-05-01 13:16:27 +0200

    setup nextjs

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+  };
+  return nextConfig;
+};
